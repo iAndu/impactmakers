ff847ac8759028c4e99d5bae88f44d9107b47160
Alexandru Banu <alex.banu14@gmail.com>
2018-03-24 11:22:55 +0200

    Install laravel Entrust

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return 'ceva';
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
