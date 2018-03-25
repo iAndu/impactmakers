447a6c9566838118049a4e909eecb92a9b4f377a
Alexandru Banu <alex.banu14@gmail.com>
2018-03-25 04:28:39 +0300

    Can update institutions

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -47,17 +47,7 @@
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">        
-          <!-- User Account: style can be found in dropdown.less -->
-          <li class="dropdown user user-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">{{ Auth::user()->name }}</span>
-            </a>
-          </li>
-        </ul>
-      </div>
+
     </nav>
   </header>
 
