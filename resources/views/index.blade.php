7c45f095b47afa5d43d5d93d0ad8e2f59ae97cdf
Andrei <andrei.zugravu97@gmail.com>
2018-03-25 06:51:50 +0300

    fixed footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -84,6 +84,14 @@
       .fix_me {
           left: 15px;
       }
+
+      #footer-wrapper {
+          padding-top: 0px;
+      }
+
+      #footer {
+          margin-top: 0px;
+      }
     </style>
 
     <!-- Modal -->
@@ -984,63 +992,6 @@
 </div>
 
 <div id="footer-wrapper">
-    <section id="bottom" class="">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-6 about-us-widget">
-                    <h4>Global Coverage</h4>
-                    <p>Was drawing natural fat respect husband. An as noisy an offer drawn blush place. These tried for way joy wrote witty. In mr began music weeks after at begin.</p>
-                </div><!--/.col-md-3-->
-
-                <div class="col-md-3 col-sm-6">
-                    <h4>Company</h4>
-                    <div>
-                        <ul class="arrow">
-                            <li><a href="#">Company Overview</a></li>
-                            <li><a href="#">Meet The Team</a></li>
-                            <li><a href="#">Our Awesome Partners</a></li>
-                            <li><a href="#">Our Services</a></li>
-                        </ul>
-                    </div>
-                </div><!--/.col-md-3-->
-
-                <div class="col-md-3 col-sm-6">
-                    <h4>Latest Articles</h4>
-                    <div>
-                        <div class="media">
-                            <div class="pull-left">
-                                <img class="widget-img" src="images/portfolio/folio01.jpg" alt="">
-                            </div>
-                            <div class="media-body">
-                                <span class="media-heading"><a href="#">Blog Post A</a></span>
-                                <small class="muted">Posted 14 April 2014</small>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <div class="pull-left">
-                                <img class="widget-img" src="images/portfolio/folio02.jpg" alt="">
-                            </div>
-                            <div class="media-body">
-                                <span class="media-heading"><a href="#">Blog Post B</a></span>
-                                <small class="muted">Posted 14 April 2014</small>
-                            </div>
-                        </div>
-                    </div>
-                </div><!--/.col-md-3-->
-
-                <div class="col-md-3 col-sm-6">
-                    <h4>Come See Us</h4>
-                    <address>
-                        <strong>Ace Towers</strong><br>
-                        New York Ave,<br>
-                        New York, 215648<br>
-                        <abbr title="Phone"><i class="fa fa-phone"></i></abbr> 546 840654 05
-                    </address>
-                </div> <!--/.col-md-3-->
-            </div>
-        </div>
-    </section><!--/#bottom-->
-
     <footer id="footer" class="">
         <div class="container">
             <div class="row">
